a1e3799151a07f5b23fae40cca24df9683cb514f
quique <elfarotello@gmail.com>
2024-11-10 15:19:05 -0500

    frontend-agregado

---
+++ front-end-login/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-400'>hola mundo</h1>
+    </>
+  )
+}
+
+export default App
